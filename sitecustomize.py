df20c7843fb46c7d3b4cfd05d5d381abf7047a82
choipasak <chlwjdgus822@gmail.com>
2026-05-29 19:15:30 +0900

    chore(deploy): Dockerfile 빌더로 통일하고 비활성 설정 제거
    - Dockerfile로 빌더 통일. 필요없는 코드 및 파일 모두 삭제.

--- sitecustomize.py
+++
@@ -1,6 +0,0 @@
-"""Python startup - force UTF-8"""
-import sys, os
-os.environ['PYTHONUTF8'] = '1'
-os.environ['PYTHONIOENCODING'] = 'utf-8'
-os.environ['LANG'] = 'C.UTF-8'
-os.environ['LC_ALL'] = 'C.UTF-8'
